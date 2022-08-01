e783f08145c17d2e35ae1d8b1c582e501ee8a193
woowang789 <wangjh789@gmail.com>
2022-08-01 17:21:13 +0900

    Replay Dto 성생

---
+++ src/main/java/com/matdongsan/web/dto/ReplyDto.java
@@ -0,0 +1,15 @@
+package com.matdongsan.web.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ReplyDto {
+    Long id;
+    Long boardId;
+    String body;
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
+
+}
