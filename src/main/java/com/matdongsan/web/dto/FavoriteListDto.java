693d3de41fc8f0d6f77240d90a7a1c92e077305b
Choisooyoung98 <ekfms15815@gmail.com>
2022-08-02 23:07:40 +0900

    FavoriteListDto 어노테이션 삭제

--- src/main/java/com/matdongsan/web/dto/FavoriteListDto.java
+++ src/main/java/com/matdongsan/web/dto/FavoriteListDto.java
@@ -1,13 +1,4 @@
 package com.matdongsan.web.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 public class FavoriteListDto {
 }
