ed92d0058aad47791f46fafe138f98b61de1d52a
Jwhyee <kawnsdud@naver.com>
2022-08-02 22:03:40 +0900

    Member Entity 구성

--- src/main/java/com/matdongsan/domain/member/Member.java
+++ src/main/java/com/matdongsan/domain/member/Member.java
@@ -1,14 +1,31 @@
 package com.matdongsan.domain.member;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
+@Getter @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(unique = true)
     private String loginId;
 
     private String loginPwd;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private LocalDateTime signUpDate;
+
+    @Enumerated(EnumType.STRING)
+    private MemberRole memberRole;
 }
