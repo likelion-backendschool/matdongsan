b564e3a0125a5868a26011990be7dd967c6e2e53
Choisooyoung98 <ekfms15815@gmail.com>
2022-08-02 16:43:09 +0900

    ReplyService.java Service어노테이션

--- src/main/java/com/matdongsan/service/ReplyService.java
+++ src/main/java/com/matdongsan/service/ReplyService.java
@@ -1,5 +1,9 @@
 package com.matdongsan.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReplyService {
     // ReplyService.java 추가 후 주석 처리
+    // @Service 어노테이션
 }
