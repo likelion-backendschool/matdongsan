40b6bfd4700f09ee1be351540b6ca0dd34c69245
Jwhyee <kawnsdud@naver.com>
2022-08-06 17:24:51 +0900

    Security를 이용한 로그인 로직 추가

--- src/main/java/com/matdongsan/web/controller/MemberController.java
+++ src/main/java/com/matdongsan/web/controller/MemberController.java
@@ -1,12 +1,16 @@
 package com.matdongsan.web.controller;
 
+import com.matdongsan.domain.member.Member;
 import com.matdongsan.service.MemberService;
-import com.matdongsan.web.dto.MemberDto;
+import com.matdongsan.web.dto.member.MemberLoginDto;
+import com.matdongsan.web.dto.member.MemberSignUpDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 
@@ -17,14 +21,25 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/login")
-    public String showSignUpPage(Model model) {
-        model.addAttribute(new MemberDto());
+    public String showLoginPage(Model model) {
+        model.addAttribute("memberLoginDto", new MemberLoginDto());
         return "member/member-login";
     }
 
-//    @GetMapping("/member/signup")
-    public String createNewMember(@Valid MemberDto memberDto) {
-        memberService.saveNewMember(memberDto);
+    @GetMapping("/signup")
+    public String showSignUpPage(Model model) {
+        model.addAttribute("memberSignUpDto", new MemberSignUpDto());
+        return "member/member-signup";
+    }
+
+    @PostMapping("/signup")
+    public String createNewMember(@Valid MemberSignUpDto memberSignUpDto, Errors errors) {
+        if (errors.hasErrors()) {
+            return "member/member-signup";
+        }
+        log.info("memberSignUpDto={}", memberSignUpDto);
+        Member member = memberService.saveNewMember(memberSignUpDto);
+        memberService.login(member);
         return "redirect:/";
     }
 }
---
+++ src/main/java/com/matdongsan/web/dto/member/MemberLoginDto.java
@@ -0,0 +1,22 @@
+package com.matdongsan.web.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class MemberLoginDto {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+}
