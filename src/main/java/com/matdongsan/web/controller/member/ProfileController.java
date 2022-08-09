7582be6bc56419c36cf8ba364c6e4751056284fb
Jwhyee <kawnsdud@naver.com>
2022-08-09 17:40:26 +0900

    fragments header-sidebar -> sidebar로 이름 변경

--- src/main/java/com/matdongsan/web/controller/member/ProfileController.java
+++ src/main/java/com/matdongsan/web/controller/member/ProfileController.java
@@ -8,18 +8,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/profile")
 @Slf4j
 public class ProfileController {
 
     private final ProfileService profileService;
 
-    @GetMapping("")
+    @GetMapping("/profile")
     public String showMyProfile(@CurrentUser Member member, Model model) {
 
         model.addAttribute("member", member);
@@ -27,16 +24,12 @@ public class ProfileController {
         return "profile/profile-main";
     }
 
-//    @GetMapping("/{username}")
-    public String showProfilePage(@CurrentUser Member member,
-                                  @PathVariable String username,
-                                  Model model) {
-        Member currentMember = profileService.findUserInfoByUserName(username);
+    @GetMapping("/profile/setting")
+    public String showProfilePage(@CurrentUser Member member, Model model) {
+
         model.addAttribute("member", member);
-        if (currentMember == null) {
-            return "redirect:/profile";
-        }
-        return "profile/profile-main";
+
+        return "profile/profile-setting";
     }
 
 }
