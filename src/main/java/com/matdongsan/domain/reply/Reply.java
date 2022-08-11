cf447d7ed1d9c77d097bcc8a3eab40cd84688134
남의영 <ny2485@naver.com>
2022-08-11 21:28:17 +0900

    Reply Post,Member 매핑, 댓글update 비즈니스 로직 추가

--- src/main/java/com/matdongsan/domain/reply/Reply.java
+++ src/main/java/com/matdongsan/domain/reply/Reply.java
@@ -1,13 +1,13 @@
 package com.matdongsan.domain.reply;
 
+import com.matdongsan.domain.member.Member;
+import com.matdongsan.domain.posts.Posts;
+import com.matdongsan.service.ReplyService;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
@@ -16,12 +16,11 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @RequiredArgsConstructor
-//자기 아이디로 들어와서 수정삭제 가능하게 어케하누
+//계정마다 관리가능하게
 public class Reply {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String comment;
 
     @CreatedDate
@@ -29,5 +28,16 @@ public class Reply {
 
     @LastModifiedDate
     private LocalDateTime modifyComment;
+
+    @ManyToOne
+    private Posts posts;
+
+    @ManyToOne
+    private Member member;
+
+    //데이터 필드를 가지고 있는 단에서 비즈니스 로직내기
+    public void updateComment(String comment) {
+        this.comment = comment;
+    }
 
 }
