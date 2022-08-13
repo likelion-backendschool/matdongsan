26aef5a2e69a5c1c160dace815048e6fec1ad572
남의영 <ny2485@naver.com>
2022-08-13 17:16:09 +0900

    (1)Post 게시물에 List형태로 댓글을 저장후 (2)댓글와 매핑되어있는 Post정보를 Reply에도 초기화

--- src/main/java/com/matdongsan/service/ReplyService.java
+++ src/main/java/com/matdongsan/service/ReplyService.java
@@ -9,9 +9,11 @@ import com.matdongsan.domain.reply.ReplyRepository;
 import com.matdongsan.web.dto.ReplyDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ReplyService {
     private final ReplyRepository replyRepository;
     private final PostsRepository postsRepository;
@@ -19,12 +21,15 @@ public class ReplyService {
 
     //댓글 저장
     public void saveReply(Member member, Posts posts, ReplyDto replyDto) {
-
+        Reply reply = Reply.builder()
+                .comment(replyDto.getComment()).build();
+        Reply savedReply = replyRepository.save(reply);     //id와 comment만 등록되어 있는 상태.
+//        posts.addReply(Reply savedReply);                 //Reply에 Post객체 초기화
     }
 
     //댓글 업데이트
-    public void updateReply(Long id, String comment) {
-        Reply replyById = replyRepository.getById(id);
+    public void updateReply(Long replyid, String comment) {
+        Reply replyById = replyRepository.getById(replyid);
         replyById.updateComment(comment);
 
     }
