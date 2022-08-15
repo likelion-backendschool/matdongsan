9510c32bfca6b6d2991f47e3a542a1258f9d187f
Jwhyee <kawnsdud@naver.com>
2022-08-15 12:30:23 +0900

    2주차 위클리 내용 추가

--- src/main/java/com/matdongsan/web/controller/member/ProfileController.java
+++ src/main/java/com/matdongsan/web/controller/member/ProfileController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
 
