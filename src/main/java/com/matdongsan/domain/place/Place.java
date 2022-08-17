719fb63f511a226b81cfc15f8e09e6ce4b3c1ec3
woowang789 <wangjh789@gmail.com>
2022-08-17 16:50:46 +0900

    create /place/place_id/detail

--- src/main/java/com/matdongsan/domain/place/Place.java
+++ src/main/java/com/matdongsan/domain/place/Place.java
@@ -1,15 +1,16 @@
 package com.matdongsan.domain.place;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Getter
 public class Place {
     @Id
@@ -23,4 +24,24 @@ public class Place {
     private String phone;
     private String x;
     private String y;
+
+    @Column(columnDefinition = "TEXT")
+    private String menus;
+    private String facilityInfo;
+
+    public Place(long id, String placeName, String placeUrl, String categoryName, String addressName, String roadAddressName, String phone, String x, String y) {
+        this.id = id;
+        this.placeName = placeName;
+        this.placeUrl = placeUrl;
+        this.categoryName = categoryName;
+        this.addressName = addressName;
+        this.roadAddressName = roadAddressName;
+        this.phone = phone;
+        this.x = x;
+        this.y = y;
+    }
+    public void setAdditionalInfo(String menu,String facility){
+        this.menus = menu;
+        this.facilityInfo = facility;
+    }
 }
--- src/main/java/com/matdongsan/service/PlaceService.java
+++ src/main/java/com/matdongsan/service/PlaceService.java
@@ -3,13 +3,24 @@ package com.matdongsan.service;
 import com.matdongsan.domain.place.Place;
 import com.matdongsan.domain.place.PlaceDto;
 import com.matdongsan.domain.place.PlaceRepository;
-import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -17,16 +28,57 @@ import java.util.Optional;
 public class PlaceService {
 
     private final PlaceRepository placeRepository;
+    private final RestTemplate restTemplate;
+    private final JSONParser parser = new JSONParser();
 
+    @Transactional
     public void savePlace(PlaceDto placeDto) {
-        if (placeRepository.existsById(placeDto.getId())) {
-            placeRepository.save(placeDto.toPlace());
+        if (!placeRepository.existsById(placeDto.getId())) {
+            Place place = placeDto.toPlace();
+            pullInfo(place);
+            placeRepository.save(place);
         }
     }
 
     public Place findPlace(long placeId) {
         Optional<Place> place = placeRepository.findById(placeId);
         return place.orElseThrow(() -> new EntityNotFoundException("해당 음식점이 없음"));
+    }
+
+    private void pullInfo(Place place) {
+
+        try {
+            String menus="";
+            String facilityInfo="";
+            String url = "https://place.map.kakao.com/main/v/" + place.getId();
+            HttpEntity<Map<String, String>> httpEntity = new HttpEntity<>(new HttpHeaders());
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+            String body = response.getBody();
+
+            JSONObject bodyJson = (JSONObject) parser.parse(body);
+            if(bodyJson.containsKey("menuInfo")){
+                JSONObject menuInfo = (JSONObject) bodyJson.get("menuInfo");
+                JSONArray menuList = (JSONArray) menuInfo.get("menuList");
+                menus = menuList.stream().map(i -> {
+                    JSONObject menuJson = (JSONObject) i;
+                    String menu = menuJson.get("menu").toString();
+                    String price = menuJson.get("price").toString();
+                    return (menu + "=" + price);
+                }).collect(Collectors.joining("//")).toString();
+            }
+            JSONObject basicInfo = (JSONObject) bodyJson.get("basicInfo");
+            if (basicInfo.containsKey("facilityInfo")) {
+                JSONObject facility = (JSONObject) basicInfo.get("facilityInfo");
+                for (Object o : facility.entrySet()) {
+                    Map.Entry<String,String> t= (Map.Entry<String, String>) o;
+                    log.info("key = {}, value = {}", t.getKey(), t.getValue());
+                }
+            }
+            place.setAdditionalInfo(menus, facilityInfo);
+        } catch (ParseException e) {
+            log.error("parser error",e);
+            throw new RuntimeException(e);
+        }
     }
 
 }
