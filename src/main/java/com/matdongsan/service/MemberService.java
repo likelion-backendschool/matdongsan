ff30f33c71d3c2e48d6ce302fe61235d89493a81
Jwhyee <kawnsdud@naver.com>
2022-08-17 15:31:41 +0900

    AccountService는 오직 로그인, 회원가입 인증/인가에 대한 로직만 다루며 MemberService는 회원 프로필과 관련된 부분들을 다룸

--- src/main/java/com/matdongsan/service/MemberService.java
+++ src/main/java/com/matdongsan/service/MemberService.java
@@ -1,98 +1,35 @@
 package com.matdongsan.service;
 
 import com.matdongsan.domain.account.Account;
-import com.matdongsan.domain.account.AccountRepository;
-import com.matdongsan.domain.account.AccountRole;
 import com.matdongsan.domain.member.Member;
 import com.matdongsan.domain.member.MemberRepository;
-import com.matdongsan.infra.SecurityUser;
-import com.matdongsan.web.dto.account.AccountSignUpDto;
-import com.matdongsan.web.vo.MemberVo;
+import com.matdongsan.web.dto.member.MemberInfoDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 @Transactional
-public class MemberService implements UserDetailsService {
+public class MemberService {
 
-    private final AccountRepository accountRepository;
     private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
+    public Member createNewMember(Account account, MemberInfoDto memberInfoDto) {
 
-    public Account saveNewMember(AccountSignUpDto accountSignUpDto) {
-        // 회원가입한 Member를 저장하는 로직
-        Account newAccount = Account.builder()
-                .username(accountSignUpDto.getUsername())
-                .password(passwordEncoder.encode(accountSignUpDto.getPassword()))
-                .email(accountSignUpDto.getEmail())
-                .build();
         Member newMember = Member.builder()
-                .birth(accountSignUpDto.getBirth())
-                .gender(accountSignUpDto.getGender())
-                .signUpDate(LocalDateTime.now())
-                .account(newAccount)
+                        .account(account)
+                        .signUpDate(LocalDateTime.now())
+                        .introduce(memberInfoDto.getIntroduce())
+                        .gender(memberInfoDto.getGender())
+                        .birth(memberInfoDto.getBirth())
                 .build();
-        memberRepository.save(newMember);
-        return accountRepository.save(newAccount);
+        account.addMember(newMember);
+        log.info("newMember={}", newMember);
+        return memberRepository.save(newMember);
     }
-
-    public void login(Account account) {
-        // Security를 이용하여 member를 로그인 시켜줌(회원가입 시에만 사용)
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
-                new SecurityUser(account),
-                account.getPassword(),
-                List.of(new SimpleGrantedAuthority("ROLE_USER")));
-        SecurityContextHolder.getContext().setAuthentication(token);
-    }
-
-    public Account findMemberByUsername(String username){
-        Optional<Account> currentMember = accountRepository.findByUsername(username);
-        return currentMember.orElseThrow(NoSuchElementException::new);
-    }
-
-    public boolean existMemberCheck(AccountSignUpDto accountSignUpDto) {
-        // 이미 존재하는 username 혹은 email 인지 확인하는 폼
-        boolean existUsername = accountRepository.existsByUsername(accountSignUpDto.getUsername());
-        boolean existEmail = accountRepository.existsByEmail(accountSignUpDto.getEmail());
-        return existUsername || existEmail;
-    }
-
-    public MemberVo getReadOnlyMember(String username) {
-        Account account = findMemberByUsername(username);
-        return MemberVo.builder()
-                .username(account.getUsername())
-                .email(account.getEmail())
-                .build();
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // 로그인을 하기 위해 가입된 member 정보를 조회
-        // UserDetailsService 구현
-
-        Optional<Account> member = accountRepository.findByUsername(username);
-
-        if (member.isEmpty()) {
-            throw new UsernameNotFoundException(username);
-        } else {
-            return new SecurityUser(member.get());
-        }
-    }
-
 }
