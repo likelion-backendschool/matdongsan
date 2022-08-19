6f513997da3ac22428eb1d87f689b2844f302a69
Jwhyee <kawnsdud@naver.com>
2022-08-19 19:21:55 +0900

    2주차 팀회고 추가

--- src/main/java/com/matdongsan/domain/account/AccountRepository.java
+++ src/main/java/com/matdongsan/domain/account/AccountRepository.java
@@ -2,9 +2,11 @@ package com.matdongsan.domain.account;
 
 import com.matdongsan.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Transactional
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByUsername(String username);
 
--- src/main/java/com/matdongsan/domain/member/MemberRepository.java
+++ src/main/java/com/matdongsan/domain/member/MemberRepository.java
@@ -2,7 +2,12 @@ package com.matdongsan.domain.member;
 
 import com.matdongsan.domain.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+
+@Transactional
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member findByAccount(Account account);
