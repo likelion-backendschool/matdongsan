c34bb87aa4a5665dd5b661eb293eb584a02f52a4
남의영 <ny2485@naver.com>
2022-08-22 13:36:48 +0900

    댓글 수정시 IllegalArgumentException 예외처리, 댓글 삭제 메소드 추가

--- src/main/java/com/matdongsan/service/ReplyService.java
+++ src/main/java/com/matdongsan/service/ReplyService.java
@@ -11,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -19,25 +21,43 @@ public class ReplyService {
     private final PostsRepository postsRepository;
     private final MemberRepository memberRepository;
 
+    public List<Reply> getReplyList(Long id) {
+        return replyRepository.findByPostsId(id);
+    }
+
+
     //댓글 저장
     public void saveReply(Member member, Posts post, ReplyDto replyDto) {
         Reply reply = Reply.builder()
                 .comment(replyDto.getComment()).build();
         Reply savedReply = replyRepository.save(reply);     //id와 comment만 등록되어 있는 상태.
         post.addReply(savedReply);                 //Reply에 Post객체 초기화
-//        member.addReply(savedReply);  //Member클래스에 reply추가 + member정보 Reply에 초기
+        member.addReply(savedReply);  //Member클래스에 reply추가 + member정보 Reply에 초기
     }
 
     //댓글 업데이트
     public void updateReply(Long replyid, String comment) {
-        Reply replyById = replyRepository.getById(replyid);
+        Reply replyById = replyRepository.findById(replyid).orElseThrow(
+                ()-> new IllegalArgumentException("댓글이 존재하지 않습니다")
+        );
         replyById.updateComment(comment);
 
     }
 
     //댓글 삭제
-    public void deleteReply() {
+    public void deleteReply(Long replyId) {
+        Reply deleteReply = replyRepository.findById(replyId).orElseThrow(
+                () -> new IllegalArgumentException("아이디가 존재하지 않습니다."));
 
+//        Posts post = postsRepository.findById(postId).orElseThrow(
+//                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
+//        );
+//
+//        Member member = memberRepository.findById(userId).orElseThrow(
+//                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
+//        );
+
+        replyRepository.deleteById(deleteReply.getId());
     }
 
 
