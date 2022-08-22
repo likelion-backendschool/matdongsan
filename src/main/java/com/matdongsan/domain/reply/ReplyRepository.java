bb1ed894d669a01e734a7de205a491307cbe8888
남의영 <ny2485@naver.com>
2022-08-22 13:35:36 +0900

    어노테이션 Repository추가, findByPostId -> findByPostsId

--- src/main/java/com/matdongsan/domain/reply/ReplyRepository.java
+++ src/main/java/com/matdongsan/domain/reply/ReplyRepository.java
@@ -1,15 +1,17 @@
 package com.matdongsan.domain.reply;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Transactional(readOnly = true)
+@Repository
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
-    List<Reply> findByPostId(Long id);
+    List<Reply> findByPostsId(Long id);
 
 
 }
