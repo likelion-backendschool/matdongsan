a694ff4c2ffdeadf82ceae0e7343bfd73aa9251d
남의영 <ny2485@naver.com>
2022-08-22 13:23:24 +0900

    findByPostId 구현 (id로 post에 있는 댓글 리스트 가져오기)

--- src/main/java/com/matdongsan/domain/reply/ReplyRepository.java
+++ src/main/java/com/matdongsan/domain/reply/ReplyRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional(readOnly = true)
-@Repository
-public interface ReplyRepository extends JpaRepository<Reply,Long> {
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    List<Reply> findByPostId(Long id);
+
+
 }
