3ef149308381558e5b79b8a63e06a1cb4656af1a
남의영 <ny2485@naver.com>
2022-08-22 13:35:04 +0900

    controller단 댓글 저장,수정,삭제 (post 주소는 수정필요)

--- src/main/java/com/matdongsan/web/controller/ReplyController.java
+++ src/main/java/com/matdongsan/web/controller/ReplyController.java
@@ -1,13 +1,58 @@
 package com.matdongsan.web.controller;
 
 
+import com.matdongsan.domain.account.Account;
+import com.matdongsan.domain.posts.Posts;
+import com.matdongsan.domain.posts.PostsRepository;
+import com.matdongsan.domain.reply.Reply;
+import com.matdongsan.domain.reply.ReplyRepository;
 import com.matdongsan.service.ReplyService;
+import com.matdongsan.web.dto.ReplyDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
 public class ReplyController {
+
     private final ReplyService replyService;
+    private final PostsRepository postsRepository;
+    private final ReplyRepository replyRepository;
+
+    @GetMapping("/post/{postId}/reply")   //post단 id연관시켜 수정필요, PathVariable
+    public List<Reply> getReplyList(@PathVariable("postId") Long id) {
+        return replyService.getReplyList(id);
+    }
+
+    @PostMapping("/post/{postId}/reply")
+    public ResponseEntity saveReply(@PathVariable("postId") Long id,
+                                    @RequestBody ReplyDto replyDto,
+                                    @AuthenticationPrincipal Account account) {
+        Posts post = postsRepository.findById(id).orElse(null);
+        if (post == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        replyService.saveReply(account.getMember(), post, replyDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/post/{postId}/reply/{replyId}")
+    public void updateReply(@PathVariable Long postId,
+                            @PathVariable Long replyId,
+                            @RequestBody ReplyDto replyDto) throws Exception {
+        replyService.updateReply(replyId, replyDto.getComment());
+    }
+
+
+    @DeleteMapping("/post/{postId}/reply/{replyId}")
+    public void deleteReply(@PathVariable Long postId,
+                              @PathVariable Long replyId,
+                              @RequestBody ReplyDto replyDto){
+        replyService.deleteReply(replyId);
+    }
 
 }
