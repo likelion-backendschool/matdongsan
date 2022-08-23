543d9700a7c07a2c906c7d6c7a730c8c8909d688
남의영 <ny2485@naver.com>
2022-08-23 10:00:04 +0900

    fetch laze로 지연로딩 설정

--- src/main/java/com/matdongsan/domain/reply/Reply.java
+++ src/main/java/com/matdongsan/domain/reply/Reply.java
@@ -21,24 +21,20 @@ public class Reply {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     @Column(nullable = false, length = 500)
     private String comment;
-
     @CreatedDate
     private LocalDateTime createComment;
-
     @LastModifiedDate
     private LocalDateTime modifyComment;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY )
 //    @JoinColumn(name = "postId")
     private Posts posts;
 
-    @ManyToOne //댓글 작성자
+    @ManyToOne(fetch = FetchType.LAZY ) //댓글 작성자
 //    @JoinColumn(name="writerId")
     private Member writer;
-
 
     //데이터 필드를 가지고 있는 단에서 비즈니스 로직내기
     public void updateComment(String comment) {
