4b0c545a2b8587b6492ccd171cf9ab3034b28ac4
Jwhyee <kawnsdud@naver.com>
2022-08-25 16:35:35 +0900

    tailwind 부분 추가

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    './src/main/**/*.{html,js}',
+    './src/main/**/*'
+  ],
+  /*theme: {
+    extend: {},
+  },
+  plugins: [],*/
+}
