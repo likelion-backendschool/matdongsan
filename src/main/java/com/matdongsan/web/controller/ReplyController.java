8644e2685cce3196eab3a242076e6bf99a0720e4
남의영 <ny2485@naver.com>
2022-08-29 22:53:07 +0900

    [Reply] 댓글등록 구현

--- src/main/java/com/matdongsan/web/controller/ReplyController.java
+++ src/main/java/com/matdongsan/web/controller/ReplyController.java
@@ -6,27 +6,46 @@ import com.matdongsan.domain.posts.Posts;
 import com.matdongsan.domain.posts.PostsRepository;
 import com.matdongsan.domain.reply.Reply;
 import com.matdongsan.domain.reply.ReplyRepository;
+import com.matdongsan.service.PostsService;
 import com.matdongsan.service.ReplyService;
 import com.matdongsan.web.dto.ReplyDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class ReplyController {
 
+    private final PostsService postsService;
     private final ReplyService replyService;
     private final PostsRepository postsRepository;
     private final ReplyRepository replyRepository;
+    ReplyDto replyDto = new ReplyDto();
+
+    //댓글등록 (버튼)
+    @PostMapping("/posts/{postId}")
+    public String createReply(@PathVariable("postId") Long id,
+                           @RequestParam String content) {
+        Posts post = postsService.findById(id);
+        ReplyDto replyDto2 = replyDto.builder().comment(content)
+                .build();
+
+        replyService.saveReply(post, replyDto2);
+        return "redirect:/posts/{postId}";
+    }
 
     @GetMapping("/post/{postId}/reply")   //post단 id연관시켜 수정필요, PathVariable
     public List<Reply> getReplyList(@PathVariable("postId") Long id) {
+
         return replyService.getReplyList(id);
     }
+
+
 
     @PostMapping("/post/{postId}/reply")
     public ResponseEntity saveReply(@PathVariable("postId") Long id,
@@ -36,7 +55,7 @@ public class ReplyController {
         if (post == null) {
             return ResponseEntity.badRequest().build();
         }
-        replyService.saveReply(account.getMember(), post, replyDto);
+//        replyService.saveReply(account.getMember(), post, replyDto);
         return ResponseEntity.ok().build();
     }
 
