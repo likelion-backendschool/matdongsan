78f365382312ff5659634c622ec6bddc10acc83a
Jwhyee <kawnsdud@naver.com>
2022-08-29 15:59:09 +0900

    [REFACTOR] #110 - Change PostCreateDto field

--- src/main/java/com/matdongsan/web/dto/posts/PostCreateDto.java
+++ src/main/java/com/matdongsan/web/dto/posts/PostCreateDto.java
@@ -2,49 +2,19 @@ package com.matdongsan.web.dto.posts;
 
 import com.matdongsan.domain.member.Member;
 import com.matdongsan.domain.posts.Posts;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data
-@NoArgsConstructor
+@Getter @Setter
 public class PostCreateDto {
-
-    private Long id;
-
-    private Member author;  // 작성자
 
     private String title; // 제목
 
     private String content; // 내용
 
-    private LocalDateTime createdTime;
-
     private LocalDateTime modifiedTime;
 
     private Boolean privateStatus;
-
-    public Posts toEntity() {
-        return Posts.builder()
-                .id(id)
-                .author(author)
-                .title(title)
-                .content(content)
-                .privateStatus(privateStatus)
-                .build();
-    }
-
-    @Builder
-    public PostCreateDto(Long id, Member author, String title, String content, LocalDateTime createdTime, LocalDateTime modifiedTime , Boolean privateStatus) {
-        this.id = id;
-        this.author = author;
-        this.title = title;
-        this.content = content;
-        this.createdTime = createdTime;
-        this.modifiedTime = modifiedTime;
-        this.privateStatus = privateStatus;
-    }
 
 }
