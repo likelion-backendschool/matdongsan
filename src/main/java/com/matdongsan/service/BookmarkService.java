503f90cb84a8442e1cd22d0e4343b4773593f660
Choisooyoung98 <ekfms15815@gmail.com>
2022-09-01 15:53:21 +0900

    [Add] #109 - 북마크 추가 기능

--- src/main/java/com/matdongsan/service/BookmarkService.java
+++ src/main/java/com/matdongsan/service/BookmarkService.java
@@ -1,5 +1,8 @@
 package com.matdongsan.service;
 
+import com.matdongsan.domain.bookmark.Bookmark;
+import com.matdongsan.domain.bookmark.BookmarkRepository;
+import com.matdongsan.domain.member.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +11,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class BookmarkService {
+
+    private final BookmarkRepository bookmarkRepository;
+    public void createBookmark(String subject, Member member) {
+        Bookmark bookmark = new Bookmark();
+        bookmark.setSubject(subject);
+        bookmark.setMember(member);
+        bookmarkRepository.save(bookmark);
+    }
 }
--- src/main/java/com/matdongsan/web/controller/bookmark/BookmarkController.java
+++ src/main/java/com/matdongsan/web/controller/bookmark/BookmarkController.java
@@ -1,9 +1,35 @@
 package com.matdongsan.web.controller.bookmark;
 
+import com.matdongsan.domain.account.Account;
+import com.matdongsan.domain.member.Member;
+import com.matdongsan.service.AccountService;
+import com.matdongsan.service.BookmarkService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.websocket.server.PathParam;
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 public class BookmarkController {
+
+    private final AccountService accountService;
+
+    private final BookmarkService bookmarkService;
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/bookmark/create")
+    public String createBookmark(Principal principal, @PathParam("subject") String subject) {
+        Account account = accountService.findAccountByUsername(principal.getName());
+        Member member = account.getMember();
+
+        bookmarkService.createBookmark(subject,member);
+
+        return "redirect:/favorite/list";
+    }
 }
