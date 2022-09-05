c46528cc9e909d6bf504c3967625a67aec025ffb
남의영 <ny2485@naver.com>
2022-09-05 13:31:37 +0900

    [Add] #131 Controller 댓글 좋아요 추가로직

--- src/main/java/com/matdongsan/web/controller/ReplyController.java
+++ src/main/java/com/matdongsan/web/controller/ReplyController.java
@@ -3,29 +3,24 @@ package com.matdongsan.web.controller;
 
 import com.matdongsan.domain.account.Account;
 import com.matdongsan.domain.account.AuthUser;
+import com.matdongsan.domain.member.Member;
 import com.matdongsan.domain.posts.Posts;
-import com.matdongsan.domain.posts.PostsRepository;
 import com.matdongsan.domain.reply.Reply;
-import com.matdongsan.domain.reply.ReplyRepository;
-import com.matdongsan.infra.SecurityUser;
+import com.matdongsan.service.MemberService;
 import com.matdongsan.service.PostsService;
 import com.matdongsan.service.ReplyService;
 import com.matdongsan.web.dto.ReplyDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.security.Principal;
-import java.util.List;
 
 @Slf4j
 @Controller
@@ -34,6 +29,7 @@ public class ReplyController {
 
     private final PostsService postsService;
     private final ReplyService replyService;
+    private final MemberService memberService;
 
     /**
      * 댓글등록 (버튼)
@@ -90,6 +86,16 @@ public class ReplyController {
     public String deleteReply(@PathVariable("replyId") Long id) {
         Reply reply = replyService.getReply(id);
         replyService.deleteReply(reply);
+        return String.format("redirect:/posts/%s", reply.getPosts().getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/reply/like/{id}")
+    public String replyLike(Principal principal, @PathVariable("id") Long id) {
+        Reply reply = replyService.getReply(id);
+        Member member = memberService.getMember(principal.getName());
+        replyService.plusReplyLike(reply,member);
+
         return String.format("redirect:/posts/%s", reply.getPosts().getId());
     }
 }
