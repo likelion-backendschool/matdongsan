a0cd54344c3f44ff7cdcc62d6577afa94c6bab32
Choisooyoung98 <ekfms15815@gmail.com>
2022-09-06 15:38:54 +0900

    [Refactor] #136 - favorite의 뷰 구성이 필요없다고 판단 profile에서 관리가 가능. -> profile-bookmark를 만들어서 기능 구현 시작

--- src/main/java/com/matdongsan/web/controller/favorite/FavoriteController.java
+++ src/main/java/com/matdongsan/web/controller/favorite/FavoriteController.java
@@ -4,20 +4,22 @@ import com.matdongsan.domain.account.Account;
 import com.matdongsan.domain.account.AuthUser;
 import com.matdongsan.domain.favorite.Favorite;
 import com.matdongsan.domain.member.Member;
-import com.matdongsan.domain.place.Place;
+import com.matdongsan.service.AccountService;
 import com.matdongsan.service.FavoriteService;
 import com.matdongsan.service.PlaceService;
+import com.matdongsan.web.vo.MemberVo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.websocket.server.PathParam;
+import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -25,19 +27,30 @@ import java.util.List;
 public class FavoriteController {
     private final FavoriteService favoriteService;
     private final PlaceService placeService;
+    private final AccountService accountService;
 
     /**
-     * favorite 뷰 이동
+     *
      * @param model
-     * @return "favorites/favorite-list"
+     * @param account
+     * @return
      */
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/favorite/list")
-    public String showFavorite(Model model, @AuthUser Account account) {
+    public String showFavorite(Model model, @AuthUser Account account, Principal principal) {
         Member member = account.getMember();
 
-        List<Favorite> favorites = favoriteService.findAllByMember(member);
-        model.addAttribute("favorites", favorites);
+        Optional<Favorite> optionalFavorite = Optional.ofNullable(favoriteService.findTopByMember(member));
+        if (optionalFavorite.isPresent()) {
+            List<Favorite> favoriteList = favoriteService.findAllByMember(member);
+            model.addAttribute("favorites", favoriteList);
+        } else {
+            Favorite favorite = new Favorite(member, "나의 맛집 리스트");
+            favoriteService.save(favorite);
+            model.addAttribute("favorites", favorite);
+        }
+        MemberVo memberVo = accountService.getReadOnlyMember(principal.getName());
+        model.addAttribute("member", memberVo);
 
         return "favorites/favorite-list";
     }
