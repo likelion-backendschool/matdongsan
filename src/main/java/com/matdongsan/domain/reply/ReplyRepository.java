831c2fde6446fdc4fc8fa0556ac7afa41b769068
남의영 <ny2485@naver.com>
2022-09-06 13:51:26 +0900

    [Add] #137 repository에서 페이징댓글 findAll

--- src/main/java/com/matdongsan/domain/reply/ReplyRepository.java
+++ src/main/java/com/matdongsan/domain/reply/ReplyRepository.java
@@ -1,5 +1,7 @@
 package com.matdongsan.domain.reply;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
@@ -12,6 +14,8 @@ import java.util.List;
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
     List<Reply> findByPostsId(Long id);
+    Page<Reply> findAll(Pageable pageable);
+
 
 
 }
