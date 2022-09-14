4aff0cb4faadbe44a479f605fb8ba0450103b4e7
Jwhyee <kawnsdud@naver.com>
2022-09-14 14:20:33 +0900

    [ADD] #162 - Ajax and Controller Interworking

--- src/main/java/com/matdongsan/web/controller/like/LikeApiController.java
+++ src/main/java/com/matdongsan/web/controller/like/LikeApiController.java
@@ -9,12 +9,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,9 +23,9 @@ public class LikeApiController {
 
     // Post
     @ResponseBody
-    @PostMapping("")
-    public boolean addNewLike(@AuthUser Account account) {
-
+    @PostMapping("/post/like/check")
+    public boolean addNewLike(@AuthUser Account account, @RequestParam Map<String, Long> params) {
+        log.info("postNum={}", params.get("postNum"));
         return true;
     }
 
