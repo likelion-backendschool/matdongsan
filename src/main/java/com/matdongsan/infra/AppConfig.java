3f8cea49368b8bc6582eaedaf21ba68a0d404fc4
woowang789 <wangjh789@gmail.com>
2022-09-15 14:43:08 +0900

    [DEBUG] #167 - rollback & add request header user-agent

--- src/main/java/com/matdongsan/infra/AppConfig.java
+++ src/main/java/com/matdongsan/infra/AppConfig.java
@@ -1,12 +1,7 @@
 package com.matdongsan.infra;
 
 import org.apache.http.client.HttpClient;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -14,11 +9,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
-import javax.net.ssl.SSLContext;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
 
 @Configuration
 public class AppConfig {
@@ -29,30 +19,12 @@ public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate(){
-        try {
-            HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
-            TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
-
-            SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
-                    .loadTrustMaterial(null, acceptingTrustStrategy)
-                    .build();
-            SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
-
-            CloseableHttpClient httpClient = HttpClients.custom()
-                    .setSSLSocketFactory(csf)
-                    .setMaxConnTotal(200)
-                    .setMaxConnPerRoute(20)
-                    .build();
-            httpRequestFactory.setConnectTimeout(2000);
-            httpRequestFactory.setReadTimeout(3000);
-//            HttpClient httpClient = HttpClientBuilder.create()
-//                    .setMaxConnTotal(200)
-//                    .setMaxConnPerRoute(20)
-//                    .build();
-            httpRequestFactory.setHttpClient(httpClient);
-            return new RestTemplate(httpRequestFactory);
-        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
-            throw new RuntimeException(e);
-        }
+        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        HttpClient httpClient = HttpClientBuilder.create()
+                .setMaxConnTotal(200)
+                .setMaxConnPerRoute(20)
+                .build();
+        httpRequestFactory.setHttpClient(httpClient);
+        return new RestTemplate(httpRequestFactory);
     }
 }
--- src/main/java/com/matdongsan/service/PlaceService.java
+++ src/main/java/com/matdongsan/service/PlaceService.java
@@ -58,8 +58,9 @@ public class PlaceService {
             String mainPhotoUrl = "";
             String url = "https://place.map.kakao.com/main/v/" + place.getId();
             HttpHeaders headers = new HttpHeaders();
-            headers.add("user-agent",
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36");
+            headers.add("Accept", "*/*");
+            headers.add("User-Agent", "PostmanRuntime/7.15.0");
+
             HttpEntity<Map<String, String>> httpEntity = new HttpEntity<>(headers);
             ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
             String body = response.getBody();
